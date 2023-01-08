174cc6dc617be02fedff2c443c818e0856e0678c
Snehasrg <sghongade642@gmail.com>
2023-01-08 22:15:07 +0530

    Find Maximum of Float

---
+++ MaxFloat.cs
@@ -0,0 +1,33 @@
+﻿namespace TestMaximum
+{
+    public class MaxFloat<T> where T : IComparable
+    {
+        public static T MaxFloatNum(T A, T B, T C)
+        {
+            if (A.CompareTo(B) > 0 && A.CompareTo(C) > 0 ||
+                A.CompareTo(B) >= 0 && A.CompareTo(C) > 0 ||
+                A.CompareTo(B) > 0 && A.CompareTo(C) >= 0)
+
+            {
+                return A;
+            }
+
+            if (B.CompareTo(A) > 0 && B.CompareTo(C) > 0 ||
+                B.CompareTo(A) >= 0 && B.CompareTo(C) > 0 ||
+                B.CompareTo(A) > 0 && B.CompareTo(C) >= 0)
+
+            {
+                return B;
+            }
+
+            if (C.CompareTo(A) > 0 && C.CompareTo(B) > 0 ||
+                C.CompareTo(A) >= 0 && C.CompareTo(B) > 0 ||
+                C.CompareTo(A) > 0 && C.CompareTo(B) >= 0)
+
+            {
+                return C;
+            }
+            return A;
+        }
+    }
+}
