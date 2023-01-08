30add84eda29962fc4f35cb7842763d5cb18ca4c
Snehasrg <sghongade642@gmail.com>
2023-01-09 00:49:10 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@
             //Console.WriteLine("Welcome to Find Maximum Problem using Generics");
 
 
-            Max3Parameter<int> obj = new Max3Parameter<int>(66, 99, 92);
+           // Max3Parameter<int> obj = new Max3Parameter<int>(66, 99, 92);
+           // obj.MaxMethod();
+
+            PrintMax<int> obj = new PrintMax<int>(66, 99, 92);
             obj.MaxMethod();
 
         }
