ddda2a8591849c02558d643d6399dcf6b7953662
Snehasrg <sghongade642@gmail.com>
2023-01-08 23:14:17 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -6,26 +6,10 @@
         {
             //Console.WriteLine("Welcome to Find Maximum Problem using Generics");
 
-            int X = 20;
-            int Y = 400;
-            int Z = 6000;
+            
 
-            int val = MaxInteger<int>.MaxIntNumber(X, Y, Z);
-            Console.WriteLine("The greater num is " + val);
-
-            float A = 40.5F;
-            float B = 50.6F;
-            float C = 80.9F;
-
-            float val1 = MaxFloat<float>.MaxFloatNum(A, B, C);
-            Console.WriteLine("The greater Float Num  is " + val1);
-
-            string P = "Red";
-            string Q = "Yellow";
-            string R = "Purple";
-
-            string val2 = MaxString<string>.MaxStringNum(P, Q, R);
-            Console.WriteLine("The greater String is " + val2);
+            int num = FindGenericVariable<int>.MaxNumber(9, 77, 200);
+            Console.WriteLine("The greater Integer number is : " + num);
         }
     }
 }
